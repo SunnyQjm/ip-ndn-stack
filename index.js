8ebde8c9ec920dcf6042c14a04dc90b14474cefa
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2018-12-17 14:51:48 +0800

    解析拉取到的以太网包

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const getDataPrefix = '/IP/';
 
 const ipPacketCache = {};
 
+const timeCalcute = {};
+
 const nfdHelper = new NFDHelper();
 const rawSocketHelper = new RawSocketHelper();
 
@@ -37,6 +39,7 @@ new PcapHelper(config.dev, config.filter)
 
         // 先将IP包缓存到内存当中
         ipPacketCache[uid] = raw_packet.buf.slice(14, packet.pcap_header.len);
+        timeCalcute[uid] = Data.now().valueOf();
 
         // 发送一个预请求Interest，提醒一个可以到达目的主机的边界网关过来拉取IP包
         nfdHelper.expressInterest(name, () => {
@@ -77,6 +80,8 @@ config.registerIp.forEach(ip => {
                 // console.log('成功拉取到数据包，开始处理数据包转发');
                 // console.log(data.getContent().buffer.length);
                 // const packet = PcapHelper.decodeIPv4Packet(data.getContent().buffer);
+                let endTime = Data.now().valueOf();
+                console.log(endTime - timeCalcute[uid]);
                 rawSocketHelper.rawSend(data.getContent().buffer, 0, data.getContent().buffer.length, destIp, function (error, bytes) {
                     if (error) {
                         console.log (error.toString ());
